9ccb169b4d49d10ff3ee20b977fb07e7354bcfff
SergioRiyo <sergioriyooshiro@gmail.com>
2026-04-28 16:51:19 -0400

    feat: configure model relationships

--- entrevista-patrimonio/app/Models/ItemEmprestimo.php
+++ entrevista-patrimonio/app/Models/ItemEmprestimo.php
@@ -3,8 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ItemEmprestimo extends Model
 {
-    //
+    protected $table = 'itens_emprestimos';
+
+    protected $fillable =
+    [
+        'emprestimo_id',
+        'patrimonio_id',
+        'data_emprestimo',
+        'data_devolucao',
+    ];
+
+    protected $casts = 
+    [
+        'data_emprestimo' => 'date',
+        'data_devolucao' => 'date',
+    ];
+
+    public function emprestimo(): BelongsTo
+    {
+        return $this->belongsTo(Emprestimo::class, 'emprestimo_id');
+    }
+
+    public function patrimonio(): BelongsTo
+    {
+        return $this->belongsTo(Patrimonio::class, 'patrimonio_id');
+    }
 }
--- entrevista-patrimonio/app/Models/Patrimonio.php
+++ entrevista-patrimonio/app/Models/Patrimonio.php
@@ -3,8 +3,39 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Patrimonio extends Model
 {
-    //
+    protected $table = 'patrimonios';
+
+    protected $fillable = [
+        'estabelecimento_pai_id',
+        'nome',
+        'codigo',
+        'tipo',
+        'data_entrada',
+        'data_baixa',
+        'motivo_baixa',
+    ];
+
+    protected $casts = [
+        'data_entrada' => 'date',
+        'data_baixa' => 'date',
+    ];
+
+    public function estabelecimentoPai() : BelongsTo
+    {
+        return $this->belongsTo(Estabelecimento::class, 'estabelecimento_pai_id');
+    }
+
+    public function itensEmprestimos()
+    {
+        return $this->hasMany(ItemEmprestimo::class, 'patrimonio_id');
+    }
+
+    public function estaBaixado(): bool
+    {
+        return ! is_null($this->data_baixa);
+    }
 }
