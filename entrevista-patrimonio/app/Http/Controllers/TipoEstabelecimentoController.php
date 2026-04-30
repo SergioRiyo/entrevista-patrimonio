4d6623bcef6782990d4baf4d5a6ad1bd0360fadb
SergioRiyo <sergioriyooshiro@gmail.com>
2026-04-30 09:54:48 -0400

    Edit/Update function fix

--- entrevista-patrimonio/app/Http/Controllers/TipoEstabelecimentoController.php
+++ entrevista-patrimonio/app/Http/Controllers/TipoEstabelecimentoController.php
@@ -45,7 +45,7 @@ class TipoEstabelecimentoController extends Controller
     /**
      * Display the specified resource.    
      */
-    public function show(string $id)
+    public function show(TipoEstabelecimento $tipoEstabelecimento)
     {
         return view('tipo-estabelecimentos.show', compact('tipoEstabelecimento'));
     }
@@ -53,7 +53,7 @@ class TipoEstabelecimentoController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(TipoEstabelecimento $tipoEstabelecimento)
     {
         return view('tipo-estabelecimentos.edit', compact('tipoEstabelecimento'));
     }
