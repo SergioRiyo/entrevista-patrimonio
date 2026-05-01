b4b29f36d2355466096e12ff3c66c41160c5343d
SergioRiyo <sergioriyooshiro@gmail.com>
2026-04-30 23:36:30 -0400

    fix: adjust form layout

--- entrevista-patrimonio/resources/views/estabelecimentos/create.blade.php
+++ entrevista-patrimonio/resources/views/estabelecimentos/create.blade.php
@@ -3,57 +3,60 @@
 @section('title', 'Novo Estabelecimento')
 
 @section('content')
-    <h2>Novo estabelecimento</h2>
+    <h2>Novo Estabelecimento</h2>
 
-    <form action="{{ route('estabelecimentos.store') }}" method="POST")>
+    <form action="{{ route('estabelecimentos.store') }}" method="POST">
         @csrf
-        <p>
-            <label for="tipo_estabelecimento_id">
-                Tipo de Estabelecimento:</label><br>
-            <select name="tipo_estabelecimento_id" id="tipo_estabelecimento_id">
 
+        <p>
+            <label for="tipo_estabelecimento_id">Tipo de Estabelecimento:</label><br>
+            <select id="tipo_estabelecimento_id" name="tipo_estabelecimento_id">
                 <option value="">Selecione</option>
 
                 @foreach ($tipos as $tipo)
-                    <option value="{{ $tipo->id }}" @selected(old('tipo_estabelecimento_id') == $tipo->id)>
+                    <option
+                        value="{{ $tipo->id }}"
+                        @selected(old('tipo_estabelecimento_id') == $tipo->id)
+                    >
                         {{ $tipo->nome }}
                     </option>
                 @endforeach
             </select>
-            @error('tipo_estabelecimento_id')
-                <br>
-                <small style="color:red">{{ $message }}</small>
-            @enderror
         </p>
+
         <p>
             <label for="nome">Nome:</label><br>
-            <input type="text" id="nome" name="nome" value="{{ old('nome') }}"
-                @error('nome')
-                <br>
-                <small style="color:red">{{ $message }}</small>
-            @enderror
-                </p>
+            <input
+                type="text"
+                id="nome"
+                name="nome"
+                value="{{ old('nome') }}"
+            >
+        </p>
 
         <p>
             <label for="cnpj">CNPJ:</label><br>
-            <input type="text" id="cnpj" name="cnpj" value="{{ old('cnpj') }}" placeholder="cnpj">
-
-            @error('cnpj')
-                <br>
-                <small style="color:red">{{ $message }}</small>)
-            @enderror
+            <input
+                type="text"
+                id="cnpj"
+                name="cnpj"
+                value="{{ old('cnpj') }}"
+                placeholder="Somente números"
+            >
         </p>
 
         <p>
-            <label for="prazo_maximo_emprestimo_dias">Prazo de emprestimo em dias:</label>
-            <input type="number" id="prazo_maximo_emprestimo_dias" name="prazo_maximo_emprestimo_dias"
-                value="{{ old('prazo_maximo_emprestimo_dias') }}">
-
-            @error('prazo_maximo_emprestimo_dias')
-                <br>
-                <small style="color:red">{{ $message }}</small>)
-            @enderror
+            <label for="prazo_maximo_emprestimo_dias">Prazo de empréstimo em dias:</label><br>
+            <input
+                type="number"
+                id="prazo_maximo_emprestimo_dias"
+                name="prazo_maximo_emprestimo_dias"
+                value="{{ old('prazo_maximo_emprestimo_dias') }}"
+                min="1"
+            >
         </p>
+
         <button type="submit">Salvar</button>
         <a href="{{ route('estabelecimentos.index') }}">Voltar</a>
     </form>
+@endsection
